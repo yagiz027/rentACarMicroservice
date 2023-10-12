61ea938727c3aaae1c1b526c5b00c343dbfd8076
Yağız ERİŞ <117725245+yagiz027@users.noreply.github.com>
2023-10-13 00:40:00 +0300

    inventory-service MaintenanceConsumer class added

--- inventory-service/src/main/java/com/yagiz/inventoryservice/business/kafka/Consumer/MaintenanceConsumer.java
+++ inventory-service/src/main/java/com/yagiz/inventoryservice/business/kafka/Consumer/MaintenanceConsumer.java
@@ -1,10 +1,12 @@
 package com.yagiz.inventoryservice.business.kafka.Consumer;
 
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 import com.yagiz.commonservice.utils.Kafka.Events.MaintenanceEvents.MaintenanceCreateEvent;
 import com.yagiz.commonservice.utils.Kafka.Events.MaintenanceEvents.MaintenanceDeleteEvent;
 import com.yagiz.inventoryservice.business.abstracts.CarService;
+import com.yagiz.inventoryservice.entity.enums.State;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,11 +17,21 @@ import lombok.extern.slf4j.Slf4j;
 public class MaintenanceConsumer {
     private final CarService service;
 
+    @KafkaListener(
+        topics = "maintenance-created",
+        groupId = "inventory-maintenance-created"
+    )
     public void consume(MaintenanceCreateEvent event){
-
+        service.changeStateByCarId(State.MAINTENANCE, event.getCarId());
+        log.info("Maintenance created and events consumed {}", event);
     }
 
+    @KafkaListener(
+        topics = "maintenance-deleted",
+        groupId = "inventory-maintenance-deleted"
+    )
     public void consume(MaintenanceDeleteEvent event){
-        
+        service.changeStateByCarId(State.AVAILABLE, event.getCarId());
+        log.info("Maintenance deleted and events consumed {}",event);
     }
 }
