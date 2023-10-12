eecce5a21af5deab8f1713a483017403f02387a4
Yağız ERİŞ <117725245+yagiz027@users.noreply.github.com>
2023-10-12 23:13:08 +0300

    inventory-service main => Paths.java import edited

--- inventory-service/src/main/java/com/yagiz/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/yagiz/inventoryservice/InventoryServiceApplication.java
@@ -3,7 +3,7 @@ package com.yagiz.inventoryservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.yagiz.commonservice.utils.constants.Paths;
+import com.yagiz.commonservice.utils.PathFinder.Paths;
 
 @SpringBootApplication(scanBasePackages = {Paths.ConfigurationBasePackage,Paths.Inventory.ServiceBasePackage})
 public class InventoryServiceApplication {
