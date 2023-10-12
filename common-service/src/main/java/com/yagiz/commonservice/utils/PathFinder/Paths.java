5ae00662d86f5ba32256fc5cc3f74797057d6005
Yağız ERİŞ <117725245+yagiz027@users.noreply.github.com>
2023-10-12 23:10:39 +0300

    common-service Paths.java location changed

--- common-service/src/main/java/com/yagiz/commonservice/utils/PathFinder/Paths.java
+++ common-service/src/main/java/com/yagiz/commonservice/utils/PathFinder/Paths.java
@@ -1,9 +1,13 @@
-package com.yagiz.commonservice.utils.constants;
+package com.yagiz.commonservice.utils.PathFinder;
 
 public class Paths {
     public static final String ConfigurationBasePackage="com.yagiz.commonservice.Configuration";
 
     public static class Inventory{
         public static final String ServiceBasePackage="com.yagiz.inventoryservice";
+    }
+
+    public static class Maintenance{
+        public static final String ServiceBasePackage="com.yagiz.maintenanceservice";
     }
 }
