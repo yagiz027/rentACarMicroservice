d435fdf06ed6c7b40c20ca7e44518bd8cb45b344
Yağız ERİŞ <117725245+yagiz027@users.noreply.github.com>
2023-10-13 00:42:00 +0300

    MaintenanceServiceApplication class scanBasePackages and EnableDiscoveryClient annotation is added

--- maintenance-service/src/main/java/com/yagiz/maintenanceservice/MaintenanceServiceApplication.java
+++ maintenance-service/src/main/java/com/yagiz/maintenanceservice/MaintenanceServiceApplication.java
@@ -2,8 +2,12 @@ package com.yagiz.maintenanceservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+import com.yagiz.commonservice.utils.PathFinder.Paths;
+
+@EnableDiscoveryClient
+@SpringBootApplication(scanBasePackages = {Paths.ConfigurationBasePackage,Paths.Maintenance.ServiceBasePackage})
 public class MaintenanceServiceApplication {
 
 	public static void main(String[] args) {
