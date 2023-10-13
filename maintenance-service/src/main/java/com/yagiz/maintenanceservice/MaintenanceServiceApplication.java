b390e440025bff5852e53435706f975ec8345638
Yağız ERİŞ <117725245+yagiz027@users.noreply.github.com>
2023-10-13 18:40:10 +0300

    FeignClient enabled

--- maintenance-service/src/main/java/com/yagiz/maintenanceservice/MaintenanceServiceApplication.java
+++ maintenance-service/src/main/java/com/yagiz/maintenanceservice/MaintenanceServiceApplication.java
@@ -3,9 +3,11 @@ package com.yagiz.maintenanceservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 import com.yagiz.commonservice.utils.PathFinder.Paths;
 
+@EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = {Paths.ConfigurationBasePackage,Paths.Maintenance.ServiceBasePackage})
 public class MaintenanceServiceApplication {
--- maintenance-service/src/main/java/com/yagiz/maintenanceservice/business/rules/MaintenanceBusinessRules.java
+++ maintenance-service/src/main/java/com/yagiz/maintenanceservice/business/rules/MaintenanceBusinessRules.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import com.yagiz.commonservice.utils.RestExceptionHandler.constants.Messages;
 import com.yagiz.commonservice.utils.RestExceptionHandler.exceptions.BusinessException;
+import com.yagiz.maintenanceservice.api.clients.CarClient;
 import com.yagiz.maintenanceservice.repository.MaintenanceRepository;
 
 import lombok.AllArgsConstructor;
@@ -12,10 +13,18 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class MaintenanceBusinessRules {
     private MaintenanceRepository repository;
+    private CarClient carClient;
 
     public void checkIfMaintenanceNotExists(int id){
         if(!repository.existsById(id)){
             throw new BusinessException(Messages.Maintenance.NotExists);
+        }
+    }
+
+    public void ensureCarIsAvailable(int carId){
+        var response = carClient.checkCarAvailibilty(carId);
+        if(!response.isSuccess()){
+            throw new BusinessException(response.getMessage());
         }
     }
 }
