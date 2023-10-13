9df138a24a6191d346d9e5ae02a8719d051bf740
Yağız ERİŞ <117725245+yagiz027@users.noreply.github.com>
2023-10-13 18:30:04 +0300

    ClientResponse classes created for the inventory-service and maintenance-service communication

---
+++ common-service/src/main/java/com/yagiz/commonservice/utils/dto/CarClientResponse.java
@@ -0,0 +1,17 @@
+package com.yagiz.commonservice.utils.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CarClientResponse extends ClientResponse {
+    private String modelName;
+    private String brandName;
+    private String plate;
+    private int modelYear;
+}
