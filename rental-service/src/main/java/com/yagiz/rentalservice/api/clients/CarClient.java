8f51f40033dbd1884eeb6e58cb81d274ecc92cd9
Yağız ERİŞ <117725245+yagiz027@users.noreply.github.com>
2023-10-19 01:44:40 +0300

    resilience4j retry annotaion added

--- rental-service/src/main/java/com/yagiz/rentalservice/api/clients/CarClient.java
+++ rental-service/src/main/java/com/yagiz/rentalservice/api/clients/CarClient.java
@@ -6,9 +6,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.yagiz.commonservice.utils.dto.ClientResponse;
 
+import io.github.resilience4j.retry.annotation.Retry;
+
 @FeignClient(name = "inventory-service", fallback = CarClientFallback.class)
 public interface CarClient {
-    
+    /**inventory-service ile iletişim kurulurken hata alındığında 
+    yapılacak iletişimin tekrar sayısını ilgili service'in config dosyasında
+    belirtmemizi sağlar. Belirtilen tekrar sayısı sonucunda iletişim başarılı
+    bir şekild sağlanamaz ise fallback içerisinde belirtilen sınıfa gidilir ve hata mesajı döndürülür.*/
+    @Retry(name = "rentalToInventory") 
     @GetMapping(value= "api/cars/check-car-available/{carId}")
     ClientResponse checkCarAvailibilty(@PathVariable int carId);
 }
