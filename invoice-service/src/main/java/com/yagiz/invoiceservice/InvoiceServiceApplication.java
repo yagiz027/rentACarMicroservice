3567ca99845e2d0a9826cba182c2d31487862f1e
Yağız ERİŞ <117725245+yagiz027@users.noreply.github.com>
2023-10-19 03:13:55 +0300

    invoice-service config base packages added

--- invoice-service/src/main/java/com/yagiz/invoiceservice/InvoiceServiceApplication.java
+++ invoice-service/src/main/java/com/yagiz/invoiceservice/InvoiceServiceApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import com.yagiz.commonservice.utils.PathFinder.Paths;
+
 @EnableFeignClients
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {Paths.ConfigurationBasePackage,Paths.Invoice.ServiceBasePackage})
 public class InvoiceServiceApplication {
 
 	public static void main(String[] args) {
