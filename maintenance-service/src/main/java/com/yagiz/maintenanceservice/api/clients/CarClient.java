ae3b3d313735c87503d886ff2bc1d3385beb1abf
Yağız ERİŞ <117725245+yagiz027@users.noreply.github.com>
2023-10-19 03:23:37 +0300

    inventory - maintenance services synchronized

--- maintenance-service/src/main/java/com/yagiz/maintenanceservice/api/clients/CarClient.java
+++ maintenance-service/src/main/java/com/yagiz/maintenanceservice/api/clients/CarClient.java
@@ -6,9 +6,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.yagiz.commonservice.utils.dto.ClientResponse;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @FeignClient(name = "inventory-service", fallback = CarClientFallback.class)
 public interface CarClient {
     
+    @CircuitBreaker(name = "maintenanceToInventory")
     @GetMapping(value= "api/cars/check-car-available/{carId}")
     ClientResponse checkCarAvailibilty(@PathVariable int carId);
 }
