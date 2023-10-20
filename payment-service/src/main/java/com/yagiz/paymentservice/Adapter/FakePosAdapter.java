f47ecd445e3ad3a60d552500e55e75845cf839b1
Yağız ERİŞ <117725245+yagiz027@users.noreply.github.com>
2023-10-21 01:58:18 +0300

    payment-service FakePosAdapter created

--- payment-service/src/main/java/com/yagiz/paymentservice/Adapter/FakePosAdapter.java
+++ payment-service/src/main/java/com/yagiz/paymentservice/Adapter/FakePosAdapter.java
@@ -1,3 +1,18 @@
-public class FakePosAdapter {
+package com.yagiz.paymentservice.adapter;
+
+import java.util.Random;
+
+import com.yagiz.commonservice.utils.RestExceptionHandler.constants.Messages;
+import com.yagiz.commonservice.utils.RestExceptionHandler.exceptions.BusinessException;
+import com.yagiz.paymentservice.business.abstracts.PosService;
+
+public class FakePosAdapter implements PosService {
+
+    @Override
+    public void pay() {
+        boolean isSuccessfull = new Random().nextBoolean();
+        if(!isSuccessfull)
+            throw new BusinessException(Messages.Payment.PaymentIsFailed);
+    }
     
 }
